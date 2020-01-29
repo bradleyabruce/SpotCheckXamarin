d2141107345e9bfcd70de0dfd5381e9c0549984c
bradleyabruce <bradleyabruce@outlook.com>
2020-01-29 15:12:08 -0500

    First Commit of Demo Project
    - Added gitignore to remove extra files
    - First upload of Microsoft provided demo project

---
+++ SpotCheck/SpotCheck/SpotCheck/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SpotCheck.Services;
+using SpotCheck.Views;
+
+namespace SpotCheck
+{
+   public partial class App : Application
+   {
+
+      public App()
+      {
+         InitializeComponent();
+
+         DependencyService.Register<MockDataStore>();
+         MainPage = new MainPage();
+      }
+
+      protected override void OnStart()
+      {
+      }
+
+      protected override void OnSleep()
+      {
+      }
+
+      protected override void OnResume()
+      {
+      }
+   }
+}
